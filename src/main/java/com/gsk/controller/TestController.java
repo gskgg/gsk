67c801e2504a56f56785ba2e7e54aaeb5215e0bb
gskgg <gsk2gg@gmail.com>
2017-09-29 02:05:17 +0530

    new changes
    new changes

---
+++ src/main/java/com/gsk/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.gsk.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/testUrl")
+public class TestController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.println("Heloo..... I am test url... and I am working fine... :) ");
+	}
+}
