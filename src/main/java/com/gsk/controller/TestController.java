00ac1a835c96f34c470af6f66b298d275f138f32
gskgg <gsk2gg@gmail.com>
2018-11-14 20:21:44 +0530

    modified TestContorller.java class

--- src/main/java/com/gsk/controller/TestController.java
+++ src/main/java/com/gsk/controller/TestController.java
@@ -16,5 +16,6 @@ public class TestController extends HttpServlet {
 			throws ServletException, IOException {
 		PrintWriter pw = resp.getWriter();
 		pw.println("Heloo..... I am test url..., and I am working fine... :) ");
+		pw.println("Heloo..... I am test url..., and I am working fine... :) ");
 	}
 }
